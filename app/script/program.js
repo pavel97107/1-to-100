7621455d1c1d88e2b4381745b25e0a0b4014a396
Pavel Siarebrany <pavel97107@gmail.com>
2020-02-02 11:07:51 +0300

    lesson6 complete

--- app/script/program.js
+++ app/script/program.js
@@ -1,6 +1,36 @@
-'uese strict';
+'use strict';
 
-let isNumber = function(n) {
-    return !isNaN(parseFloat(n)) && isFinite(n); // парс конвертирует в строку или в число, !isNan если число то возвращет тру анологично фолс
-}; // принимает число вовращает тру или фолс, // isFinite если число конечное(не является бесконечным) то возвращается тру
+function guess() {
+  let isNumber = function (n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  };
 
+  let randomNumber = Math.floor(Math.random() * Math.floor(100)) + 1;
+  console.log('Рандомное число: ', randomNumber);
+
+  let checkNumber = function() {
+      let userNumber = +prompt('Угадай число от 1 до 100?');
+      if (userNumber === null) {
+          alert('Игра закончена');
+      } else if (isNumber(userNumber)) {
+          userNumber += userNumber;
+          if(userNumber > randomNumber) {
+              alert('Загаданное число меньше');
+              checkNumber();
+          } else if ( userNumber < randomNumber) {
+              alert('Загаданное число больше');
+              checkNumber();
+          } else  {
+              alert('Вы победили');
+              return;
+          }
+      } else {
+          alert('Введи число!');
+          checkNumber();
+        }   
+    }
+    return checkNumber;
+
+}
+let guessNumber = guess();
+guessNumber();
